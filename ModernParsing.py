2ce377d1261e8ad1fa47d22badfe0b7f6ecc093b
Sapron12 <44747416+Sapron12@users.noreply.github.com>
2018-11-13 16:36:30 +0300

    Update 2

--- ModernParsing.py
+++ ModernParsing.py
@@ -3,6 +3,7 @@
 from lxml import html
 from lxml.html import html5parser
 import requests
+
 # а теперь хуясь, кодировочка установлена
 #Тут я тестил, как отличается lxml от bs4. Понял только, что bs4 удобмнее
     
@@ -15,6 +16,4 @@ strd = html.fromstring(r.content)
 
 result = strd.xpath('''//*[@id="f-756"]/td/h4/a/text()''')
 proxy_list = result
-print(r)
-print(strd)
 print(result)
