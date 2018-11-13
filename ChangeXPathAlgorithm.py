1eab13ac76bdff5cf14c00a24f0c13cc2aef78e2
medvedko <celason@gmail.com>
2018-11-14 02:19:59 +0300

    Update ChangeXPathAlgorithm.py
    Убрал костыль из алгоритма, вот теперь, кажется, у нас с ним все хорошо
    Но я уже спать хочу, поэтому мог что то упустить
    Обязательно займись этим, проверь, все ли работает как надо

--- ChangeXPathAlgorithm.py
+++ ChangeXPathAlgorithm.py
@@ -1,26 +1,26 @@
 a = '//*[@id="dle-content"]/div[1]/div[1]/h2/a'
-q = 0
 nums = '1234567890'
 myList = []
 print(a)
-try:
-    for x in a:
-        if a[q] == '[':
-            if a[q+1] in nums:
+q = 0
+
+while q != len(a):
+    if a[q] == '[':
+        q += 1
+        if a[q] in nums:
+            print(a[q], 'MISS', q)
+            while a[q] != ']':
+                q += 1
                 print(a[q], 'MISS', q)
-                while a[q] != ']':
-                    q += 1
-                    print(a[q], 'MISS', q)
-                q += 1
-            else:
-                myList.append(a[q])
-                print(a[q], 'ADDED', q)
-                q += 1
+            q += 1
         else:
             myList.append(a[q])
             print(a[q], 'ADDED', q)
             q += 1
-except:
-    myString = ''.join(myList)
-    print(myString)
+    else:
+        myList.append(a[q])
+        print(a[q], 'ADDED', q)
+        q += 1
+myString = ''.join(myList)
+print(myString)
 print("")
