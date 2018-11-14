fd77760562822a2eb0dcff570897ed8a38699287
medvedko <celason@gmail.com>
2018-11-14 11:40:42 +0300

    Update COMN/ChangeXPathAlgorithm.py

--- COMN/ChangeXPathAlgorithm.py
+++ COMN/ChangeXPathAlgorithm.py
@@ -1,26 +1,27 @@
 a = '//*[@id="dle-content"]/div[1]/div[1]/h2/a'
 nums = '1234567890'
-myList = []
 print(a)
-q = 0
+my_list = []
 
-while q != len(a):
-    if a[q] == '[':
-        q += 1
-        if a[q] in nums:
-            print(a[q], 'MISS', q)
-            while a[q] != ']':
+
+def xpath():
+    q = 0
+    while q != len(a):
+        if a[q] == '[':
+            q += 1
+            if a[q] in nums:
+                while a[q] != ']':
+                    q += 1
                 q += 1
-                print(a[q], 'MISS', q)
-            q += 1
+            else:
+                my_list.append(a[q])
+                q += 1
         else:
-            myList.append(a[q])
-            print(a[q], 'ADDED', q)
+            my_list.append(a[q])
             q += 1
-    else:
-        myList.append(a[q])
-        print(a[q], 'ADDED', q)
-        q += 1
-myString = ''.join(myList)
-print(myString)
-print("")
+    my_string = ''.join(my_list)
+    print("")
+    print(my_string)
+
+
+xpath()
