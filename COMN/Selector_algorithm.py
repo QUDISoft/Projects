7f705dc99768dbe6aae387e0d89b25d0bdecce99
medvedko <celason@gmail.com>
2018-11-14 11:57:23 +0300

    Update COMN/Selector_algorithm.py

--- COMN/Selector_algorithm.py
+++ COMN/Selector_algorithm.py
@@ -1,14 +1,14 @@
 a = '#comment_126060840 > div.comment__body > div.comment__content > p:nth-child(2)'
 q = 0
 myList = []
+print(a)
 while q != len(a):
     if a[q] == '>':
-        print(a[q], 'MISS')
         q += 2
     else:
         myList.append(a[q])
-        print(a[q], 'ADDED')
         q += 1
 
 myString = ''.join(myList)
+print("")
 print(myString)
