1ec210388e108cd27c2106a86c60843448beaf87
Sapron12 <44747416+Sapron12@users.noreply.github.com>
2018-11-16 02:47:08 +0300

    Алгоритм, который пригодился

--- COMN/Selector_algorithm.py
+++ COMN/Selector_algorithm.py
@@ -1,4 +1,4 @@
-a = '#comment_126060840 > div.comment__body > div.comment__content > p:nth-child(2)'
+a = '#container > div > div.l-gradient-wrapper > div > div > div.news-content.js-scrolling-area > div.news-wrapper > div > div > div > div > div.news-grid__part.news-grid__part_1 > div.news-tidings > div > div > div:nth-child(1) > div.news-tidings__clamping > div.news-tidings__speech.news-helpers_hide_mobile-small'
 q = 0
 myList = []
 print(a)
